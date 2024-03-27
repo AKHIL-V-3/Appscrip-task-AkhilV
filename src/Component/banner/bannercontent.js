15856e37436f8a24274c1d8a2d42a0da607984b3
Akhil V <akhilv033@gmail.com>
2024-03-27 20:50:18 +0530

    header completed

--- src/Component/banner/bannercontent.js
+++ src/Component/banner/bannercontent.js
@@ -1,1 +1,20 @@
+import '../../Styles/banner.css'
+const Banner = () => {
 
+    return (
+        <>
+
+          <div className="banner">
+
+              <div className='banner-content'>
+                 <h1 className='banner-h1'>DISCOVER OUR PRODUCTS</h1>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque porro ipsa delectus tempore iusto. Harum praesentium ut dolorum sapiente blanditiis doloribus Lorem ipsum dolor sit amet consectetur</p>
+              </div>
+
+          </div>
+
+        </>
+    )
+}
+
+export default Banner
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import '../styles/global.css';
 import Header from "../Component/header/header"
+import Banner from '../Component/banner/bannercontent';
 
 export default function Home() {
   return (
     <main>
       <div>
         <Header/>
+        <Banner/>
       </div>
     </main>
   );
